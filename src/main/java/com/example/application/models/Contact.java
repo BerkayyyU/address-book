608cfdfd1e8078e94724784e0bf0a35336da6bcf
Berkay Ulguel <berkayyyulguel@gmail.com>
2021-06-23 22:28:44 +0300

    simple grid with static contact names

---
+++ src/main/java/com/example/application/models/Contact.java
@@ -0,0 +1,24 @@
+package com.example.application.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String company;
+
+}
--- src/main/java/com/example/application/views/MainView.java
+++ src/main/java/com/example/application/views/MainView.java
@@ -1,20 +1,33 @@
 package com.example.application.views;
 
+import com.example.application.models.Contact;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Route
 public class MainView extends VerticalLayout {
-
+    List<Contact> contactList = new ArrayList<>();
     public MainView(){
-        Button button = new Button("Vaadin button");
+        Grid<Contact> grid = new Grid<>(Contact.class);
+        grid.setItems(contactList);
 
-        button.addClickListener(buttonClickEvent -> {
-            Notification.show("Button clicked");
-        });
-        add(button);
+        grid.removeColumnByKey("id");
+        grid.removeColumnByKey("lastName");
+        grid.removeColumnByKey("company");
+
+        contactList.add(new Contact(1L,"Berkay","Ulguel","Company"));
+        contactList.add(new Contact(1L,"Ali","Ulguel","Company"));
+        contactList.add(new Contact(1L,"Mehmet","Ulguel","Company"));
+        contactList.add(new Contact(1L,"Bilmemne","Ulguel","Company"));
+
+        grid.setColumns("firstName");
+        add(grid);
     }
 
 }
